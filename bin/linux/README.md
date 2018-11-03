c0f1eb054d1c0c87867d8cb2a17e69eb5f2f5f27
Joe Cai <joey.cai@gmail.com>
2018-11-03 17:47:25 +1100

    add vpn_nat.sh

---
+++ bin/linux/README.md
@@ -0,0 +1,1 @@
+This directory contains small scripts for linux servers, for backup purposes. Unlike other files in this repo, they are not supposed to work on a Mac system.
---
+++ bin/linux/vpn_nat.sh
@@ -0,0 +1,20 @@
+ #!/bin/bash
+
+VPN_CIDR=192.168.199.0/24
+LAN_CIDR=10.0.0.0/24
+IFACE_OUT=eth0
+IFACE_VPN=tun0
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+iptables -F
+iptables -t nat -F
+ # Allow traffic initiated from VPN to access local network
+iptables -I FORWARD -i $IFACE_VPN -o $IFACE_OUT \
+     -s $VPN_CIDR -d $LAN_CIDR -m conntrack --ctstate NEW -j ACCEPT
+
+# Allow established traffic to pass back and forth
+iptables -I FORWARD -m conntrack --ctstate RELATED,ESTABLISHED \
+     -j ACCEPT
+
+iptables -t nat -I POSTROUTING -o IFACE_OUT -s $VPN_CIDR  -j MASQUERADE
